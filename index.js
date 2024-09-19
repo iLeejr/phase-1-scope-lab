3e1593aed6bee9cd71956a5fa8302b4209317186
Kaleb Malone <kalebmalone7@gmail.com>
2024-09-19 10:10:12 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+var writeBestCustomer = 'maybe bob'
+
+function overwriteBestCustomer() {
+    bestCustomer = writeBestCustomer
+}
+
+const leastFavoriteCustomer = 'probably bob'; 
+
+function changeLeastFavoriteCustomer(newName) {
+    leastFavoriteCustomer = newName; 
+}
+
